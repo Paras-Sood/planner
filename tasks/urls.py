6c68353b2aa7d46beba5c4926f78926e5140ecbe
Paras <soodparassood4@gmail.com>
2021-04-02 14:41:22 +0530

    Added hyperlinks for each task and some bugs fixed

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name="index"),
+    path('<int:task_id>',views.task_view,name="task_view"),
     path('add/',views.add,name="add"),
     path('login/',views.login_view,name="login"),
     path('logout/',views.logout_view,name="logout"),
--- tasks/views.py
+++ tasks/views.py
@@ -9,30 +9,9 @@ from django import forms
 from . import util
 from .models import User, task
 import datetime
-# class NewTaskForm(forms.Form):
-#     name=forms.CharField(label="name")
-
-# Create your views here.
-# def index(request):
-#     content=[]
-#     entries=util.list_entries()
-#     for entry in entries:
-#         content.append(util.get_entry(entry))
-#         print(util.get_entry(entry))
-#     return render(request, "tasks/index.html",{
-#         "tasks":util.list_entries(),
-#         "content":content
-#     })
 
 def index(request):
-    # return render(request, "tasks/index.html")
-    # if request.method == "POST":
-    # uname=request.POST["username"]
-    # print(f"uname {uname}")
-    # user=User.objects.get(username=uname)
-    # print(f"user {user}")
     return render(request,"tasks/index.html")
-    # return HttpResponse("<h1>Here</h1>")
 
 def add(request):
     # pass
@@ -48,7 +27,7 @@ def add(request):
         # User.task.add()
         # ntask=task.objects.create()
         tsk = task(sdate= sdate,edate= edate,stime= stime,etime= etime,title= title,description= description,owner=owner)
-        print(f"Task {tsk}")
+        # print(f"Task {tsk}")
         tsk.save()
         owner.tasks.add(tsk)
         return HttpResponseRedirect(reverse('index'))
@@ -70,14 +49,11 @@ def login_view(request):
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
-        test=User.objects.get(username=username)
-        print(f"User = {user}")
-        print(f"Test = {test.password}")
+        # print(f"User = {user}")
         # Check if authentication successful
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse('index'))
-            # return HttpResponseRedirect(reverse("index"))
         else:
             return render(request, "tasks/login.html", {
                 "message": "Invalid username and/or password."
@@ -88,15 +64,14 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("index"))
+    return HttpResponseRedirect(reverse('index'))
 
 
 def register(request):
     if request.method == "POST":
-        print("IN")
         uname = request.POST["username"]
         em = request.POST["email"]
-        print("Got")
+        # print("Got")
         # Ensure password matches confirmation
         pswd = request.POST["password"]
         confirmation = request.POST["confirmation"]
@@ -104,25 +79,32 @@ def register(request):
             return render(request, "tasks/register.html", {
                 "message": "Passwords must match."
             })
-        print("Try")
+        # print("Try")
+
         # Attempt to create new user
         try:
             # user = User.objects.create(username, email, password)
             user = User.objects.create_user(username=uname,password=pswd,email=em)
             user.save()
-            print(f"Saved {user}")
-            test = authenticate(request, username=uname, password=pswd)
-            print(f"User = {test}")
+            # print(f"Saved {user}")
+            # test = authenticate(request, username=uname, password=pswd)
+            # print(f"User = {test}")
         except IntegrityError:
             return render(request, "tasks/register.html", {
                 "message": "Username already taken."
             })
         # user = User(username=username,email=email,password=password)
         # user.save()
-        print(f"After {user.username}")
+        # print(f"After {user.username}")
         # return HttpResponseRedirect(reverse("login"))
-        login(request, User.objects.get(username=uname))
-        print("Login")
+        login(request, user)
+        # print("Login")
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "tasks/register.html")
+
+def task_view(request,task_id):
+    tsk=task.objects.get(pk=task_id)
+    return render(request,"tasks/task.html",{
+        "task":tsk
+    })

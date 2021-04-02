543c8bea04d2b0e65ced4bc5cca7d770924aea1f
Paras <soodparassood4@gmail.com>
2021-04-02 16:48:13 +0530

    Security Issues Fixed

--- tasks/views.py
+++ tasks/views.py
@@ -14,7 +14,6 @@ def index(request):
     return render(request,"tasks/index.html")
 
 def add(request):
-    # pass
     if request.method == "POST":
         title=request.POST["title"]
         sdate=request.POST['sdate']
@@ -24,22 +23,10 @@ def add(request):
         description=request.POST['description']
         user_id=request.POST['user_id']
         owner=User.objects.get(pk=user_id)
-        # User.task.add()
-        # ntask=task.objects.create()
         tsk = task(sdate= sdate,edate= edate,stime= stime,etime= etime,title= title,description= description,owner=owner)
-        # print(f"Task {tsk}")
         tsk.save()
         owner.tasks.add(tsk)
         return HttpResponseRedirect(reverse('index'))
-        # form=NewTaskForm(request.POST)
-        # if form.is_valid():
-        #     name=form.cleaned_data["name"]
-        #     start=request.POST["start"]
-        #     end=request.POST["end"]
-        #     content= start + " ------------------------------------- " + end
-        #     print(content)
-        #     util.save_entry(name,content)
-        #     return HttpResponseRedirect(reverse(index))
     return render(request, "tasks/add.html")
 
 def login_view(request):
@@ -49,7 +36,7 @@ def login_view(request):
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
-        # print(f"User = {user}")
+
         # Check if authentication successful
         if user is not None:
             login(request, user)
@@ -71,7 +58,6 @@ def register(request):
     if request.method == "POST":
         uname = request.POST["username"]
         em = request.POST["email"]
-        # print("Got")
         # Ensure password matches confirmation
         pswd = request.POST["password"]
         confirmation = request.POST["confirmation"]
@@ -79,31 +65,22 @@ def register(request):
             return render(request, "tasks/register.html", {
                 "message": "Passwords must match."
             })
-        # print("Try")
-
         # Attempt to create new user
         try:
-            # user = User.objects.create(username, email, password)
             user = User.objects.create_user(username=uname,password=pswd,email=em)
             user.save()
-            # print(f"Saved {user}")
-            # test = authenticate(request, username=uname, password=pswd)
-            # print(f"User = {test}")
         except IntegrityError:
             return render(request, "tasks/register.html", {
                 "message": "Username already taken."
             })
-        # user = User(username=username,email=email,password=password)
-        # user.save()
-        # print(f"After {user.username}")
-        # return HttpResponseRedirect(reverse("login"))
         login(request, user)
-        # print("Login")
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "tasks/register.html")
 
 def task_view(request,task_id):
+    # temp=User.objects.get(pk=user_id)
+    # tsk=task.objects.get(pk=task_id,owner=temp)
     tsk=task.objects.get(pk=task_id)
     return render(request,"tasks/task.html",{
         "task":tsk
